648dfb17ed11c202228b1509b021a62e7f94ad96
Andrii <andrey.paladin@gmail.com>
2019-09-22 11:19:22 +0300

    commit build

---
+++ precache-manifest.6856c6d7bd1025ba59f23f1c923e5b91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f971758238aeebd48e6dd4e1d941fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ef281246d41399fa87c",
+    "url": "/static/css/main.8afceaeb.chunk.css"
+  },
+  {
+    "revision": "6ad41db2a2c901a7a4df",
+    "url": "/static/js/2.1ce9be25.chunk.js"
+  },
+  {
+    "revision": "9ef281246d41399fa87c",
+    "url": "/static/js/main.824c27a5.chunk.js"
+  },
+  {
+    "revision": "a659c513b913be4e7b37",
+    "url": "/static/js/runtime-main.d52357dd.js"
+  }
+]);
